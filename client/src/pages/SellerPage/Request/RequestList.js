b3ac9867ed741504be1052f0fb7ad2fb91a39c0c
kdj <rlaehdwp7451@gmail.com>
2020-09-17 10:36:16 +0900

    kdj commit

--- client/src/pages/SellerPage/Request/RequestList.js
+++ client/src/pages/SellerPage/Request/RequestList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Request from './Request';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
@@ -38,49 +38,56 @@ const RequestList = ({ category }) => {
 
     const [data, setData] = useState([]);
 
+    const [tags, setTags] = useState([]);
+
     const [loading, setLoading] = useState(true);
 
     const getAllRequests = useCallback(() => {
-        if(category === "모든 요청") {
-            Axios.get('/requests/' + page)
-            .then(res => {
-                let requests = res.data.requestList;
-                const tags = res.data.tags;
-                requests = requests.map((element,index) => {
-                    element.tags = tags[index];
-                });
-                setData(requests);
-                setTotalPage(res.data.count);
-                setLoading(false);
+        if (category === "모든 요청") {
+            Axios.get('/requests/' + page, {
+                tags: tags
             })
+                .then(res => {
+                    let requests = res.data.requestList;
+                    const tags = res.data.tags;
+                    requests = requests.map((element, index) => {
+                        element.tags = tags[index];
+                    });
+                    setData(requests);
+                    setTotalPage(res.data.count);
+                    setLoading(false);
+                })
 
         }
         else {
-        Axios.get('/requests/category/' + page)
-            .then(res => {
-                let requests = res.data.requestList;
-                const tags = res.data.tags;
-                requests = requests.map((element,index) => {
-                    element.tags = tags[index];
-                });
-                setData(requests);
-                setTotalPage(res.data.count);
-                setLoading(false);
+            Axios.get('/requests/category/' + page, {
+                tags: tags
             })
-            .catch(err => {
-                console.log(err);
-            })
+                .then(res => {
+                    let requests = res.data.requestList;
+                    const tags = res.data.tags;
+                    requests = requests.map((element, index) => {
+                        element.tags = tags[index];
+                    });
+                    setData(requests);
+                    setTotalPage(res.data.count);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
-    },[category,page])
+    }, [category, page,tags])
+
 
     useEffect(() => {
         getAllRequests();
         return () => {
             setLoading(true);
+            setTags([]);
         }
     }, [getAllRequests])
 
-    const [tags, setTags] = useState([]);
 
     const tagSort = tags.map((obj, index) => {
         return <Chip className={classes.tagStyle} key={index} label={obj} variant="default" size="small" onDelete={() => { onClickRemove(index) }} />
@@ -101,8 +108,8 @@ const RequestList = ({ category }) => {
     }
 
     const requestList = data.map((obj) => {
-        
-           
+
+
         return (
             <Grid key={obj.request_id} item xs={12} sm={12} md={6}>
                 <Request tags={tags} setTags={setTags} data={obj} checked={loading}></Request>
@@ -121,14 +128,14 @@ const RequestList = ({ category }) => {
             {totalPage
                 ?
                 <>
-                <Grid container spacing={3}>
-                    {requestList}
-                </Grid>
-                <Pagination className={classes.pagination} count={totalPage} page={page} onChange={handleChangePage} defaultPage={1} />
+                    <Grid container spacing={3}>
+                        {requestList}
+                    </Grid>
+                    <Pagination className={classes.pagination} count={totalPage} page={page} onChange={handleChangePage} defaultPage={1} />
                 </>
                 :
-                <Typography variant="h5" style={{marginTop:'40px'}}>진행중인 요청이 없습니다.</Typography>
-                }
+                <Typography variant="h5" style={{ marginTop: '40px' }}>진행중인 요청이 없습니다.</Typography>
+            }
         </Container>
     )
 
